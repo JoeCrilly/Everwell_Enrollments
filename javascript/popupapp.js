c09c81558a676fa89ed637d18a197b5b0b104e52
chhaya <chhayachhaya27dp@yahoo.com>
2015-12-11 12:15:28 -0800

    added javascript files

---
+++ javascript/popupapp.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$('.popup-youtube').magnificPopup({
+		disableOn: 0,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+});
+
